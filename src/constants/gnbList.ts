af4a053d9713cc31ca3b225a56d5e260803669de
DevTeddy123 <ab315601@naver.com>
2025-09-24 22:31:05 +0900

    feat

--- src/constants/gnbList.ts
+++ src/constants/gnbList.ts
@@ -1,18 +1,22 @@
 export const URL_LIST = [
-  {
-    url: "/#Intro",
-    label: "Tax Agent 소개",
-  },
-  {
-    url: "/#Features",
-    label: "주요 기능",
-  },
-  {
-    url: "/#Security",
-    label: "보안",
-  },
-  {
-    url: "/#Pricing",
-    label: "요금 안내",
-  },
-];
+	{
+		url: '/#Intro',
+		label: 'Tax Agent 소개',
+	},
+	{
+		url: '/#Features',
+		label: '주요 기능',
+	},
+	{
+		url: '/#Security',
+		label: '보안',
+	},
+	{
+		url: '/#Pricing',
+		label: '요금 안내',
+	},
+	{
+		url: '/pdf',
+		label: 'pdf test',
+	},
+]
